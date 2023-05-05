461d4627e79fc74af8ed6f4496b514f1d28db06d
Gustavo <gustavo.cassaro@edu.ufes.br>
2023-05-05 16:46:44 -0300

    add t01

---
+++ TESTE_01.vhd
@@ -0,0 +1,16 @@
+entity TESTE_01 is 
+end entity;
+
+architecture sim of TESTE_01 is
+
+begin
+
+    process is
+    begin
+
+        report "Hello World!";
+        wait;
+
+    end process;
+
+end architecture;
