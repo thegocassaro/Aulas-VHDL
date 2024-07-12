6ded9be635d7fc171ab74b22a00dfa0037aeb8b9
Gustavo Cassaro <128737494+thegocassaro@users.noreply.github.com>
2024-07-12 09:24:31 -0300

    Delete t3 directory

--- t3/libraries.vhd
+++
@@ -1,7 +0,0 @@
---in every project there will always be a library declared by default, implicitly: the work library
---it can be used by every file of your project in any level so that every structure you make can be used anywhere
-
---any library is gonna contain a number of packages
-
-library ieee;
-use ieee.std_logic_1164.all; --the "all" means we are gonna use all of the package (std_logic_1164)
